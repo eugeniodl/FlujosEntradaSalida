bf7ea9758d847cebe50048131fc5eaf9a8ac9e5a
eugeniodl <Eugenio.Diaz@fcys.uni.edu.ni>
2023-03-17 15:03:06 -0600

    Agregar PruebaArchivos y PruebaMemoryStream

--- PruebaArchivos/Program.cs
+++ PruebaArchivos/Program.cs
@@ -1,44 +1,39 @@
-﻿Stream fs = new FileStream("clientes.txt", FileMode.Open,
-    FileAccess.Read);
+﻿Stream fs = new FileStream("clientes.txt", FileMode.Open, FileAccess.Read);
 
-// obtenemos el número de bytes
+// Obtenemos el número de bytes 
 long cantidad = fs.Length;
 Console.WriteLine($"El archivo tiene una longitud de {cantidad} bytes");
 
-//for (long cont = 0; cont < cantidad; cont++)
-//{
-//    // Establecemos la posición
-//    fs.Seek(cont, SeekOrigin.Begin);
-//    int valor = fs.ReadByte();
-//    Console.WriteLine($"Posición {cont}: {(char)valor}");
-//}
-
-//Console.WriteLine("-------------------------");
-
-//for(long cont = cantidad; cont > 0; cont--)
-//{
-//    // Establecemos la posición
-//    fs.Seek(-cont, SeekOrigin.End);
-//    int valor = fs.ReadByte();
-//    Console.WriteLine($"Posición {cont}: {(char)valor}");
-//}
-
-int valor2 = 0;
-
-while(valor2 != -1)
+for (long cont = 0; cont < cantidad; cont++)
 {
-    valor2 = fs.ReadByte();
-    if(valor2 != -1)
-        Console.WriteLine($"{(char)valor2}");
+    // Establecemos la posición
+    fs.Seek(cont, SeekOrigin.Begin);
+    int valor = fs.ReadByte();
+    Console.WriteLine($"Posición {cont}: {(char)valor}");
+}
+
+Console.WriteLine("------------------------------");
+for (long cont = cantidad; cont > 0; cont--)
+{
+    // Establecemos la posición
+    fs.Seek(-cont, SeekOrigin.End);
+    int valor = fs.ReadByte();
+    Console.WriteLine($"Posición {cont}: {(char)valor}");
 }
 
 fs.Seek(0, SeekOrigin.Begin);
 
+
 StreamReader sr = new StreamReader(fs);
+
+while(!sr.EndOfStream)
+{
+    Console.WriteLine(sr.ReadLine());
+}
 
 string todo = sr.ReadToEnd();
 Console.WriteLine(todo);
 
-// MUY IMPORTANTE: Cerrar el Stream
+//  MUY IMPORTANTE: Cerrar el Stream
 sr.Close();
 fs.Close();
--- PruebaMemoryStream/Program.cs
+++ PruebaMemoryStream/Program.cs
@@ -1,7 +1,7 @@
 ﻿using(Stream ms = new MemoryStream())
 {
-    using(Stream fs = new FileStream("clientes.txt", FileMode.Open,
-        FileAccess.Read))
+    using(Stream fs = new FileStream("clientes.txt",
+        FileMode.Open, FileAccess.Read))
     {
         fs.CopyTo(ms);
     }
@@ -10,7 +10,7 @@
 
     using(StreamReader sr = new StreamReader(ms))
     {
-        while(!sr.EndOfStream)
+        while (!sr.EndOfStream)
         {
             Console.WriteLine(sr.ReadLine());
         }
@@ -21,7 +21,7 @@
     {
         using(StreamWriter sw = new StreamWriter(fs))
         {
-            sw.WriteLine("200,Maria,Lopez,2000");
+            sw.WriteLine("200,Alicia,Espinoza,14523");
         }
     }
 }
