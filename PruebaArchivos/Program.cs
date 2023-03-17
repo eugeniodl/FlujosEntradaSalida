20a15dce6d7ab312ce8789017ebdae6e4a7d5d67
eugeniodl <Eugenio.Diaz@fcys.uni.edu.ni>
2023-03-17 14:35:55 -0600

    Agregar PruebaArchivos y PruebaMemoryStream

---
+++ PruebaArchivos/Program.cs
@@ -0,0 +1,39 @@
+﻿Stream fs = new FileStream("clientes.txt", FileMode.Open, FileAccess.Read);
+
+// Obtenemos el número de bytes 
+long cantidad = fs.Length;
+Console.WriteLine($"El archivo tiene una longitud de {cantidad} bytes");
+
+for (long cont = 0; cont < cantidad; cont++)
+{
+    // Establecemos la posición
+    fs.Seek(cont, SeekOrigin.Begin);
+    int valor = fs.ReadByte();
+    Console.WriteLine($"Posición {cont}: {(char)valor}");
+}
+
+Console.WriteLine("------------------------------");
+for (long cont = cantidad; cont > 0; cont--)
+{
+    // Establecemos la posición
+    fs.Seek(-cont, SeekOrigin.End);
+    int valor = fs.ReadByte();
+    Console.WriteLine($"Posición {cont}: {(char)valor}");
+}
+
+fs.Seek(0, SeekOrigin.Begin);
+
+
+StreamReader sr = new StreamReader(fs);
+
+while(!sr.EndOfStream)
+{
+    Console.WriteLine(sr.ReadLine());
+}
+
+string todo = sr.ReadToEnd();
+Console.WriteLine(todo);
+
+//  MUY IMPORTANTE: Cerrar el Stream
+sr.Close();
+fs.Close();
