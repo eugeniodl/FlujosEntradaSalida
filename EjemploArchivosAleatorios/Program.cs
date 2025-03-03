205b7c338290087d058826a18c1344b9d44a0f6b
eugeniodl <Eugenio.Diaz@fcys.uni.edu.ni>
2025-03-03 17:47:12 -0600

    Ejemplos de archivos secuenciales y aleatorios

---
+++ EjemploArchivosAleatorios/Program.cs
@@ -0,0 +1,13 @@
+﻿string path = "datos.bin";
+using (BinaryWriter bw = new BinaryWriter(File.Open(path, FileMode.Create)))
+{
+    bw.Write(100);
+    bw.Write(200);
+}
+
+using (BinaryReader br = new BinaryReader(File.Open(path, FileMode.Open)))
+{
+    int num1 = br.ReadInt32();
+    int num2 = br.ReadInt32();
+    Console.WriteLine($"Números leídos: {num1}, {num2}");
+}
