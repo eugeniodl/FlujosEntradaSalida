268bbd578f15ad0b95a6652607dda9af294aef98
eugeniodl <Eugenio.Diaz@fcys.uni.edu.ni>
2026-06-05 08:14:49 -0600

    RegistroEventos

--- RegistroEventos/Program.cs
+++ RegistroEventos/Program.cs
@@ -1,2 +1,62 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿string rutaArchivo = "eventos.log";
+
+EventoRepository repository = new EventoRepository(rutaArchivo);
+EventoService service = new EventoService(repository);
+
+Console.WriteLine("--- REGISTRO DE EVENTOS ---");
+Console.WriteLine();
+
+Console.WriteLine("Historial de eventos");
+
+try
+{
+	List<string> historial = service.ObtenerHistorial();
+
+	if (historial.Count == 0)
+		Console.WriteLine("No existen eventos registrados.");
+	else
+	{
+		foreach (string evento in historial)
+		{
+			Console.WriteLine(evento);
+		}
+	}
+
+	Console.WriteLine();
+	Console.WriteLine("--- NUEVO EVENTO ---");
+
+	Console.Write("Nombre de usuario: ");
+	string usuario = Console.ReadLine()!;
+
+	Console.Write("Descripción del evento: ");
+	string descripcion = Console.ReadLine()!;
+
+	service.RegistrarEvento(usuario, descripcion);
+
+	Console.WriteLine();
+	Console.WriteLine("Evento registrado correctamente.");
+}
+catch (FileNotFoundException)
+{
+    Console.WriteLine("El archivo no existe.");
+}
+catch (DirectoryNotFoundException)
+{
+	Console.WriteLine("La ruta especificada no es válida.");
+}
+catch(UnauthorizedAccessException)
+{
+	Console.WriteLine("No posee permisos suficientes.");
+}
+catch(IOException ex)
+{
+	Console.WriteLine($"Error de acceso al archivo: {ex.Message}");
+}
+catch(ArgumentException ex)
+{
+	Console.WriteLine(ex.Message);
+}
+
+Console.WriteLine();
+Console.WriteLine("Presione una tecla para salir...");
+Console.ReadKey();
