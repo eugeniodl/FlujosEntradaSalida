7775967d6038c66579e04d29f797aa0b3cd2aa1c
eugeniodl <eugenio.diaz@fcys.uni.edu.ni>
2026-06-09 09:36:24 -0600

    SistemaBiblioteca

--- SistemaBiblioteca/Program.cs
+++ SistemaBiblioteca/Program.cs
@@ -1,2 +1,38 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿ICatalagoRepository repository = 
+    new CatalogoXmlRepository("catalogo.xml");
+
+CatalogoService service = new CatalogoService(repository);
+
+CatalogoLibros catalogo = new()
+{
+    Biblioteca = "Biblioteca Central"
+};
+
+catalogo.Libros.Add(
+    new Libro
+    {
+        Titulo = "C# Avanzado",
+        Autor = "Juan Pérez",
+        AnioPublicacion = 2020,
+        Precio = 29.99m
+    });
+
+catalogo.Libros.Add(
+    new Libro
+    {
+        Titulo = "POO en Profundidad",
+        Autor = "María Gómez",
+        AnioPublicacion = 2019,
+        Precio = 24.99m
+    });
+
+service.GuardarCatalago(catalogo);
+
+CatalogoLibros recuperado = service.ObtenerCatalogo();
+
+Console.WriteLine($"Biblioteca: {recuperado.Biblioteca}");
+
+foreach (Libro libro in recuperado.Libros)
+{
+    Console.WriteLine($"{libro.Titulo} - {libro.Autor}");
+}
