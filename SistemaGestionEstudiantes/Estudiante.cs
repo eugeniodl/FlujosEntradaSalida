788c6d0192f6ed9a3914585ada2b38b267ac1b5c
eugeniodl <Eugenio.Diaz@fcys.uni.edu.ni>
2026-06-09 12:06:57 -0600

    SistemaGestionEstudiante

---
+++ SistemaGestionEstudiantes/Estudiante.cs
@@ -0,0 +1,50 @@
+﻿public class Estudiante
+{
+    private string _carnet = string.Empty;
+    private string _nombre = string.Empty;
+    private double _promedio;
+
+    public string Carnet
+    {
+        get => _carnet;
+        set
+        {
+            if(string.IsNullOrWhiteSpace(value))
+                throw new ArgumentException("Carnet inválido.");
+            _carnet = value;
+        }
+    }
+
+    public string Nombre
+    {
+        get => _nombre;
+        set
+        {
+            if (string.IsNullOrWhiteSpace(value))
+                throw new ArgumentException("Nombre inválido.");
+            _nombre = value;
+        }
+    }
+
+    public double Promedio
+    {
+        get => _promedio;
+        set
+        {
+            if (value < 0 || value > 100)
+                throw new ArgumentOutOfRangeException(
+                    nameof(Promedio),
+                    "El promedio debe estar entre 0 y 100.");
+            _promedio = value;
+        }
+    }
+
+    public Estudiante(string carnet, string nombre, 
+        double promedio)
+    {
+        Carnet = carnet;
+        Nombre = nombre;
+        Promedio = promedio;
+    }
+}
+
---
+++ SistemaGestionEstudiantes/EstudianteJsonRepository.cs
@@ -0,0 +1,87 @@
+﻿
+using System.Text.Json;
+
+public class EstudianteJsonRepository : IEstudianteRepository
+{
+    private readonly string _archivo;
+
+    public EstudianteJsonRepository(string archivo)
+    {
+        _archivo = archivo;
+
+        if (!File.Exists(_archivo))
+            File.WriteAllText(_archivo, "[]");
+    }
+
+    public void Actualizar(Estudiante estudiante)
+    {
+        List<Estudiante> estudiantes = LeerArchivo();
+
+        Estudiante existente =
+            estudiantes.FirstOrDefault(
+                e => e.Carnet == estudiante.Carnet)
+            ?? throw new ArgumentException(
+                "Estudiante no encontrado.");
+
+        existente.Nombre = estudiante.Nombre;
+        existente.Promedio = estudiante.Promedio;
+
+        GuardarArchivo(estudiantes);
+    }
+
+    private void GuardarArchivo(List<Estudiante> estudiantes)
+    {
+        string json = JsonSerializer.Serialize(estudiantes,
+            new JsonSerializerOptions
+            {
+                WriteIndented = true,
+            });
+        File.WriteAllText(_archivo, json);
+    }
+
+    private List<Estudiante> LeerArchivo()
+    {
+        string json = File.ReadAllText(_archivo);
+
+        return JsonSerializer.Deserialize<List<Estudiante>>(json)
+            ?? new List<Estudiante>();
+    }
+    
+    public void Agregar(Estudiante estudiante)
+    {
+        List<Estudiante> estudiantes = LeerArchivo();
+
+        if (estudiantes.Any(e => e.Carnet == estudiante.Carnet))
+            throw new InvalidOperationException(
+                "Ya existe un estudiante con ese carnet.");
+        estudiantes.Add(estudiante);
+        GuardarArchivo(estudiantes);
+    }
+
+    public void Eliminar(string carnet)
+    {
+        List<Estudiante> estudiantes = LeerArchivo();
+
+        Estudiante estudiante = 
+            estudiantes.FirstOrDefault(
+                e => e.Carnet == carnet)
+            ?? throw new ArgumentException(
+                "Estudiante no encontrado.");
+        estudiantes.Remove(estudiante);
+        GuardarArchivo(estudiantes);
+    }
+
+    public Estudiante ObtenerPorCarnet(string carnet)
+    {
+        return LeerArchivo().
+            FirstOrDefault(e => e.Carnet == carnet)
+            ?? throw new ArgumentException(
+                "Estudiante no encontrado.");
+    }
+
+    public List<Estudiante> ObtenerTodos()
+    {
+        return LeerArchivo();
+    }
+}
+
