ed2e35246dddb62edc2423b35e543fae932da09a
eugeniodl <Eugenio.Diaz@fcys.uni.edu.ni>
2026-06-10 14:15:05 -0600

    SistemaBiblioteca

---
+++ SistemaBiblioteca/Libro.cs
@@ -0,0 +1,25 @@
+﻿public class Libro
+{
+    private string _titulo = string.Empty;
+    private string _autor = string.Empty;
+
+    public string Titulo
+    {
+        get => _titulo;
+        set => _titulo = string.IsNullOrWhiteSpace(value)
+            ? throw new ArgumentException("Título inválido")
+            : value;
+    }
+
+    public string Autor
+    {
+        get => _autor;
+        set => _autor = string.IsNullOrWhiteSpace(value)
+            ? throw new ArgumentException("Autor inválido")
+            : value;
+    }
+
+    public int AnioPublicacion { get; set; }
+    public decimal Precio {  get; set; }
+}
+
