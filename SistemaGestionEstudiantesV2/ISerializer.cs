10c6a88f108cc4c5fb0ef83dd8d4cf1ecfdfd35f
eugeniodl <Eugenio.Diaz@fcys.uni.edu.ni>
2026-06-16 08:36:18 -0600

    SistemaGestionEstudiantesV2

---
+++ SistemaGestionEstudiantesV2/ISerializer.cs
@@ -0,0 +1,6 @@
+﻿public interface ISerializer<T>
+{
+    void Guardar(List<T> datos, string rutaArchivo);
+    List<T> Cargar(string rutaArchivo);
+}
+
---
+++ SistemaGestionEstudiantesV2/XmlFileSerializer.cs
@@ -0,0 +1,38 @@
+﻿using System.Xml.Serialization;
+
+public class XmlFileSerializer<T> : ISerializer<T>
+{
+    public List<T> Cargar(string rutaArchivo)
+    {
+        if (!File.Exists(rutaArchivo))
+            return new();
+
+        XmlSerializer serializer =
+            new(typeof(List<T>));
+
+        using FileStream fs = new(rutaArchivo, FileMode.Open,
+            FileAccess.Read);
+
+        return (List<T>)serializer.Deserialize(fs)!;
+    }
+
+    public void Guardar(List<T> datos, string rutaArchivo)
+    {
+        try
+        {
+            XmlSerializer serializer = new(typeof(List<T>));
+
+            using FileStream fs = new(rutaArchivo, FileMode.Create,
+                FileAccess.Write);
+
+            serializer.Serialize(fs, datos);
+        }
+        catch (UnauthorizedAccessException ex)
+        {
+            throw new IOException(
+                "No tiene permisos para escribir el archivo.",
+                ex);
+        }
+    }
+}
+
